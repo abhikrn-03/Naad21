71b84900d450b37daa02b333f3f990a394347f79
abhikrn_03 <abhik333333@gmail.com>
2021-03-10 03:51:53 +0530

    Marketing mail added

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const Admin = require('./../models/admin');
 const Event = require('./../models/event');
 const Registration = require('./../models/registration');
+const sendEmail = require('./../utils/sendEmail');
 
 router.get('/', async (req, res) => {
     return res.render('login');
@@ -51,6 +52,13 @@ router.get('/findById', (req, res) => {
     res.render('findById');
 });
 
+router.get('/markMail', (req, res) => {
+    if (req.isAuthenticated() === false) {
+        res.redirect('login');
+    }
+    res.render('markMail');
+});
+
 
 router.post('/findEvent', async (req, res) => {
 
@@ -96,6 +104,28 @@ router.post('/findById', async (req, res) => {
             res.render('findByIdResult', { participants: data });
         }
     });
+});
+
+router.post('/markMail', async (req, res) => {
+    try {
+        participants = await Registration.find({});
+        let link = "https://naad21.in/naadwhitelogo.png";
+        for (let x in participants){
+            sendEmail(x.email, req.body.mailSubject, "<div style='text-align:center'><img src=" + link + "></div>" + "<div>" + req.body.mailBody + "</div>" + "<br><br> See you around, till then, stay musical!<br><br> Team NAAD");
+        }
+        // To reduce server calls:
+        // let recipients = "'"; let i=0;
+        // const nosrecp = participants.length;
+        // for (i=0; i<(nosrecp-1); i++){
+        //     recipients = recipients + participants[i] + "' ";
+        // }
+        // recipients = recipients + participants[nosrecp-1];
+        // sendEmail(recipients, req.body.mailSubject, "<div style='text-align:center'><img src=" + link + "></div>" + "<div>" + req.body.mailBody + "</div>" + "<br><br> See you around, till then, stay musical!<br><br> Team NAAD");
+    } catch(err) {
+        console.log(err);
+        res.status(400).redirect('markMail');
+    }
+
 });
 
 module.exports = router;
