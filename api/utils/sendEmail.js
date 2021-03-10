3df5310aeba8eedb252d787ca4fa5493719ce15f
root <root@vm1-apci-naad21.txtbequ34lbefkptnzy21t4z0e.rx.internal.cloudapp.net>
2021-03-10 17:21:05 +0000

    Emails secured

--- api/utils/sendEmail.js
+++ api/utils/sendEmail.js
@@ -11,17 +11,19 @@ const sendEmail = async (email, subject, msg) => {
             secure: true,
             pool: true
         });
-
+	console.log("Transport created.");
         const message = {
             from: process.env.emailUsername,
             to: email,
             subject: subject, // Subject line
             html: `<p>${msg}</p>`
         };
+
+	console.log("Message body created");
         //var resp = false;
         transport.sendMail(message, function (err, info) {
             if (err) {
-                console.log(err);
+                console.log("There has been an error:",err);
                 resolve(false);
             } else {
                 console.log("Email Sent");
